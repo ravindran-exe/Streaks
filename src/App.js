bffa25167ce9fe8dcb3398704ca4200f70946e06
ravindran-exe-mac <ravindran2213003@ssn.edu.in>
2024-11-27 11:36:37 +0530

    fix: import issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
